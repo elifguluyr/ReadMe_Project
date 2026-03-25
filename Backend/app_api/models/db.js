dffc31593231650ee9d2dc996ccf22583025ad82
elifguluyr <uyarelif2005@gmail.com>
2026-03-25 11:49:21 +0300

    db.js güncellendi

--- Backend/app_api/models/db.js
+++ Backend/app_api/models/db.js
@@ -1,6 +1,5 @@
 var mongoose=require('mongoose');
-//var dbURI='mongodb+srv://TrioReads:TrioReads123@cluster0.epc63ez.mongodb.net/?appName=Cluster0/ReadMeDB';
-var dbURI='mongodb://localhost/ReadMeDB';
+var dbURI='mongodb+srv://TrioReads:TrioReads123@cluster0.epc63ez.mongodb.net/?appName=Cluster0/ReadMeDB';
 mongoose.connect(dbURI);
 mongoose.connection.on('connected',function(){
     console.log(dbURI+' adresindeki veritabanına bağlanıldı.');
