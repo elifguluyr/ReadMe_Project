2bfe3dd4eaf870c861429700d79d0ec4a0513998
elifguluyr <uyarelif2005@gmail.com>
2026-03-28 22:13:23 +0300

    Frontend Backend bağlantısı güncellendi

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://readme-api-m7pb.onrender.com',
         changeOrigin: true,
       }
     }
